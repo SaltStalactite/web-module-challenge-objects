58fa4bd406ade8db7dceef256d63080b42ff832d
cc <votaryofturms@icloud.com>
2021-08-12 18:04:38 -0400

    stretch 1

--- index.js
+++ index.js
@@ -159,10 +159,16 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
-function getReviewByRating(/* code here */) {
-  /* code here */
+function getReviewByRating(array, rating) {
+  let ratingArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].rating >= rating && array[i].rating < rating + 1) {
+      ratingArray.push(array[i]);
+    }
+  }
+  return ratingArray;
 }
-
+console.log('stretch 1', getReviewByRating(reviews, 4));
 
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪💪   
 Use the getLongReviews function below to do the following:
